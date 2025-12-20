61b5be112258a373a82e69312af67038d179dffd
yyxff <salmontwosday@gmail.com>
2025-12-19 23:52:47 -0500

    feat/fetch: Add github api for repo meta data like stars

---
+++ src/utils/github-api.ts
@@ -0,0 +1,12 @@
+import fetch from "node-fetch";
+
+
+export async function fetchRepoMeta(owner: string, repo: string) {
+    const url = `https://api.github.com/repos/${owner}/${repo}`;
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch github repo meta: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+}
