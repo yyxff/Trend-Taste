be8d951ece96d6b27674049ce663c73c6e572418
yyxff <salmontwosday@gmail.com>
2025-12-21 23:03:24 -0500

    feat/fetch: Better comments

--- src/utils/github-api.ts
+++ src/utils/github-api.ts
@@ -1,6 +1,11 @@
 import fetch from "node-fetch";
 
-
+/**
+ * Fetches the meta data of a repository by GitHub API
+ * @param owner the owner of the repository
+ * @param repo the name of the repository
+ * @returns the meta data of the repository
+ */
 export async function fetchRepoMeta(owner: string, repo: string) {
     const url = `https://api.github.com/repos/${owner}/${repo}`;
     const response = await fetch(url);
--- src/utils/github-rss-api.ts
+++ src/utils/github-rss-api.ts
@@ -1,6 +1,9 @@
 import fetch from "node-fetch";
 
-
+/**
+ * Fetches the RSS data of the GitHub trending repositories by 3rd party RSS API
+ * @returns the RSS data of the trending repositories
+ */
 export async function fetchGithubTrending() {
     const url = "https://mshibanami.github.io/GitHubTrendingRSS/daily/all.xml";
     const response = await fetch(url);
